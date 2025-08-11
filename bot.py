0d92eea8506547c7ab744592b8d992a132cafbfc
Aether <hollowofaether@gmail.com>
2025-08-11 12:46:44 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -117,6 +117,8 @@ class EventCog(commands.Cog):
             "- **!eventreg** - Log an event\n"
             " • Example: `!eventreg @User Cooking false 7/25`\n"
             " • Example: `!eventreg @User PVP true 1st 7/25`\n"
+            "- **!editentry - Edit an entry of an event\n"
+            "• Example: `!editentry @User Cooking 5/6 => Cooking 5/6/2024`\n"
             "- **!clearall [@user]** — Clear all stats for a user\n"
             "- **!clearrec [@user]** — Clear most recent stat for a user\n"
             "- **!marathonset @User <number>** — Set Marathon Wins for a user\n"
