67fd14f7ab87a26970d00ae4b48f853d79f95c79
Aether <hollowofaether@gmail.com>
2025-08-14 00:32:50 -0400

    Update drop.py

--- drop.py
+++ drop.py
@@ -1,12 +1,1 @@
-import asyncio
-import asyncpg
-import os
 
-async def drop_table():
-    DATABASE_URL = os.getenv("DATABASE_URL")
-    conn = await asyncpg.connect(DATABASE_URL)
-    await conn.execute("DROP TABLE team_members;")
-    print("Table dropped")
-    await conn.close()
-
-asyncio.run(drop_table())
